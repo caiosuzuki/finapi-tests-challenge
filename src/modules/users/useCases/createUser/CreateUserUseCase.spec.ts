f9f289bb067a92dd3a4834c1f564c44adb857970
caiosuzuki <caiosuzuki@digix.com.br>
2022-06-13 20:15:05 -0400

    creating tests for the create user use case

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { CreateUserError } from "./CreateUserError";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let usersRepository: IUsersRepository;
+
+describe("Create user", () => {
+    beforeEach(() => {
+        usersRepository = new InMemoryUsersRepository();
+        createUserUseCase = new CreateUserUseCase(usersRepository);
+    })
+
+    it("should be able to create a user", async () => {
+        const name = "Johnny";
+        const email = "johnny@email.com";
+        const password = "johnnyspassword123";
+
+        const user = await createUserUseCase.execute({name, email, password});
+
+        expect(user.name).toEqual(name);
+        expect(user.email).toEqual(email);
+    })
+
+    it("should be able to create a user", async () => {
+        expect(async () => {
+            const name = "Johnny";
+            const email = "johnny@email.com";
+            const password = "johnnyspassword123";
+    
+            await createUserUseCase.execute({name, email, password});
+            const user = await createUserUseCase.execute({name, email, password});
+        }).rejects.toBeInstanceOf(CreateUserError);
+    })
+})
